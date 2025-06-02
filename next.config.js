3aade82f10f6346a3b8cbb5711852af1dd32979b
Angel Francisco <ang3lfco@outlook.com>
2025-06-01 23:21:28 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
+module.exports = nextConfig

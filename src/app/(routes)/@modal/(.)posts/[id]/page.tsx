65bbe6c5b7cc962982282de7dde91b46b2fcc9ac
ang3lfco <ang3lfco@outlook.com>
2025-06-02 01:16:02 -0700

    testing solution

--- src/app/(routes)/@modal/(.)posts/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)posts/[id]/page.tsx
@@ -3,12 +3,18 @@ import ModalPostContent from "@/components/ModalPostContent";
 import Preloader from "@/components/Preloader";
 import { Suspense } from "react";
 
-export default function PostInModal({params:{id}}:{params:{id:string}}){
-    return(
-        <Modal>
-            <Suspense fallback={<Preloader/>}>
-                <ModalPostContent postId={id}/>
-            </Suspense>
-        </Modal>
-    );
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function PostInModal({ params: { id } }: Props) {
+  return (
+    <Modal>
+      <Suspense fallback={<Preloader />}>
+        <ModalPostContent postId={id} />
+      </Suspense>
+    </Modal>
+  );
 }
